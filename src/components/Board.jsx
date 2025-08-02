e833b9a51d35290c7ee809472cd98e83c0a13d4e
Lisa Louison <lisa.louison@gmail.com>
2025-08-02 15:29:40 -0500

    styles(tailwind): adding basic tailwind styles

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,7 +5,7 @@ const Board = () => {
   const [board, setBoard] = useState(Array(6).fill(null));
 
   return (
-    <div>
+    <div className='flex items-center justify-around'>
       {board.map((value, index) => (
         <Card key={index} value={value} />
       ))}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className='m-10 rounded-lg bg-[#F7B2B7] px-6 py-8 shadow-xl ring-1 dark:bg-black'>
+      <p>Card</p>
+    </div>
+  );
 };
 
 export default Card;
