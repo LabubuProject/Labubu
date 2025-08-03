af70f6e914f8e709d8a4de689ccead8287de2372
Lisa Louison <lisa.louison@gmail.com>
2025-08-03 12:51:57 -0500

    feat(stats): add stats to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Stats from './Stats.jsx';
 
-const Header = () => {
+const Header = ({ flips, gameWon, gameStarted }) => {
   //hamburger menu?
   return (
     <div className='flex flex-col items-center justify-center mt-10'>
-      <h1 className='text-3xl font-bold underline'>Labu Who?</h1>
+      <h1 className='text-3xl font-semibold mb-8 text-[#4B3A18]'>Labu Who?</h1>
+      {gameStarted && (
+        <Stats flips={flips} gameWon={gameWon} gameStarted={gameStarted} />
+      )}
     </div>
   );
 };
