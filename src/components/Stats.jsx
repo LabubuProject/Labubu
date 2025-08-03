9094f9334c8304e46683c97535d0d00fa7c432ba
Lisa Louison <lisa.louison@gmail.com>
2025-08-03 12:52:49 -0500

    docs(stats): add comments for future features

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const Stats = ({ flips, gameWon }) => {
+const Stats = ({ flips, gameWon, gameStarted }) => {
+  //use gameStarted for timer logic?
+  //add user name to win message?
   return (
-    <div>
+    <div className='text-center'>
       <p>Timer: 0:00</p>
       <p>{`Number of Flips: ${flips}`}</p>
       {gameWon && <p>You Win!</p>}
