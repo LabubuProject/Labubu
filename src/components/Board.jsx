a821362b56e56a774a6ae1c744e3b5fdb359de08
Lisa Louison <lisa.louison@gmail.com>
2025-08-02 19:59:35 -0500

    style(cards): center cards on board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,7 +5,7 @@ const Board = () => {
   const [board, setBoard] = useState(Array(6).fill(null));
 
   return (
-    <div className='flex items-center justify-around'>
+    <div className='grid grid-cols-3 gap-2'>
       {board.map((value, index) => (
         <Card key={index} value={value} />
       ))}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = () => {
   return (
-    <div className='m-10 rounded-lg bg-[#F7B2B7] px-6 py-8 shadow-xl ring-1 dark:bg-black'>
+    <div className='m-2 text-center rounded-lg bg-[#F7B2B7] px-6 py-8 shadow-xl ring-1 dark:bg-black'>
       <p>Card</p>
     </div>
   );
