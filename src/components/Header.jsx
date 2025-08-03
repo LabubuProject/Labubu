be328f6eeb181ad943b66aa246e83a15fb37388e
Lisa Louison <lisa.louison@gmail.com>
2025-08-03 01:14:32 -0500

    style(header): style header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   //hamburger menu?
   return (
-    <div className='flex items-center justify-center mt-10'>
+    <div className='flex flex-col items-center justify-center mt-10'>
       <h1 className='text-3xl font-bold underline'>Labu Who?</h1>
     </div>
   );
