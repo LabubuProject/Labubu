595e8ecf69b6d1743fee334db4fda50bf4ef0650
Lisa Louison <lisa.louison@gmail.com>
2025-08-03 20:47:37 -0500

    fix(html): added missing html attributes and update colors to match styleguide

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -27,23 +27,25 @@ export default function Login() {
           <h2 className='text-2xl font-semibold text-center text-ebony mb-6'>
             Log In
           </h2>
-          <label htmlFor='username' className='block text-ebony mb-1'>
+          <label htmlFor='username' className='block text-ebony mb-1 text-lg'>
             Username
             <input
               type='text'
               id='username'
               name='username'
+              className='ml-1 min-w-[230px] border-2 rounded-md border-gray-100 focus:border-teal-500 focus:outline-none'
               value={username}
               onChange={(e) => setUsername(e.target.value)}
               required
             />
           </label>
-          <label htmlFor='password' className='block text-ebony mb-1'>
+          <label htmlFor='password' className='block text-ebony mb-1 text-lg'>
             Password
             <input
               type='password'
               id='password'
               name='password'
+              className='ml-1 min-w-[235px] border-2 rounded-md border-gray-100 focus:border-teal-500 focus:outline-none'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
@@ -51,12 +53,15 @@ export default function Login() {
           </label>
           <button
             type='submit'
-            className='w-full bg-[#A1D6D4] text-white py-2 rounded hover:bg-[#41A5A4] focus:outline-none focus:ring-2 focus:ring-[#41A5A4] transition'
+            className='w-full text-white py-2 rounded bg-[#A1D6D4] hover:bg-[#41A5A4] focus:ring-[#41A5A4] focus:outline-none focus:ring-2 transition'
           >
             Log In
           </button>
           <p className='text-center text-sm text-ebony mt-4'>
-            Don’t have an account? <Link to='/signup'>Sign up</Link>
+            Don’t have an account?{' '}
+            <Link className='underline' to='/signup'>
+              Sign up
+            </Link>
           </p>
         </form>
       </div>
