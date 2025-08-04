2b9d430cfd5551515f2da504c5e4909259c35896
Lisa Louison <lisa.louison@gmail.com>
2025-08-03 22:00:38 -0500

    style(cards): add images for card flips

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,30 +1,19 @@
 import '../index.css';
 
 const Card = ({ index, value, onClick, selectedCards, matchedCards }) => {
+  // check if this card is selected or matched to determine conditional content and styling
   const isSelectedCard = selectedCards.find((card) => card.index === index);
   const isMatchedCard = matchedCards.find((card) => card.index === index);
   const flipped = isSelectedCard || isMatchedCard;
 
-  // check if this card is selected or matched to determine conditional content and styling
-  // const btnContent = isSelectedCard || isMatchedCard ? value : '?';
-  // const btnStyle = isMatchedCard ? 'bg-[#FFFFFF]' : 'bg-[#F7B2B7]';
-
   return (
-    // <button
-    //   //if card is matched, button is disabled
-    //   disabled={isMatchedCard}
-    //   //if card is selected, click listener is disabled
-    //   onClick={isSelectedCard ? null : () => onClick(index, value)}
-    //   className={`m-2 text-center rounded-lg bg-gradient-to-br from-[#f7b2b7] to-[#D89B9E]  hover:bg-[#dd906f] px-6 py-8 shadow-lg hover:shadow-xl dark:bg-black ${btnStyle}`}
-    // >
-    //   {btnContent}
-    // </button>
     <div
+      //if card is selected, click listener is disabled
       className='card w-24 h-32 m-2'
-      onClick={!flipped ? () => onClick(index, value) : undefined}
+      onClick={!flipped ? () => onClick(index, value) : null}
     >
       <div className={`card-inner ${flipped ? 'flipped' : ''}`}>
-        <div className='card-front rounded-lg shadow-lg flex items-center justify-center bg-gradient-to-br from-pink-200 to-pink-300 text-xl'>
+        <div className='card-front rounded-lg shadow-lg flex items-center justify-center bg-gradient-to-br from-[#f7b2b7] to-[#D89B9E] text-ebony text-xl'>
           ?
         </div>
         <div className='card-back rounded-lg shadow-lg flex items-center justify-center bg-white text-3xl'>
