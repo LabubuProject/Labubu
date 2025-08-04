ce6e33b45dba6fc71e7ee6fc14fc0515abb94898
Lisa Louison <lisa.louison@gmail.com>
2025-08-03 20:21:50 -0500

    style(title): update title styling to white

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,9 @@ const Header = ({ flips, gameWon, gameStarted }) => {
   //hamburger menu?
   return (
     <div className='flex flex-col items-center justify-center mt-10'>
-      <h1 className='text-3xl font-semibold mb-8 text-[#4B3A18]'>Labu Who?</h1>
+      <h1 className='text-3xl font-semibold mb-8 text-white text-shadow-lg/30'>
+        Labu Who?
+      </h1>
       {gameStarted && (
         <Stats flips={flips} gameWon={gameWon} gameStarted={gameStarted} />
       )}
