ba5b9f693821c6fce8be3a1405e0df6a896145ab
yuan-cloud <eucapop@gmail.com>
2025-08-05 13:55:08 -0700

    feat(methods) : temp scoreboard functions

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -61,4 +61,50 @@ userController.verifyUser = async (req, res, next) => {
   }
 };
 
+//adding new method updateUserScore for new feature Scoreboard
+userController.updateUserScore = async (req, res, next) => {
+  const { bestTime, highestLevel } = req.body;
+  const username = req.user?.username; // provided by tokenController.verifyUserToken, or maybe use ._id?
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    // Update only if new score is better
+    if (!user.bestTime || bestTime < user.bestTime) {
+      user.bestTime = bestTime;
+    }
+    if (!user.highestLevel || highestLevel > user.highestLevel) {
+      user.highestLevel = highestLevel;
+    }
+
+    const updatedUser = await user.save();
+    res.locals.user = updatedUser;
+    return next();
+  } catch (err) {
+    return next({
+      log: 'Error in updateUserScore',
+      status: 500,
+      message: { err: 'Failed to update user score' },
+    });
+  }
+};
+// this below is for after the Dropdown Scoreboard layout is done
+// userController.getScoreboard = async (req, res, next) => {
+//   try {
+//     const scoreboard = await User.find()
+//       .sort({ highestLevel: -1, bestTime: 1 })
+//       .select('username highestLevel bestTime -_id')
+//       .limit(10);
+//     res.locals.scoreboard = scoreboard;
+//     return next();
+//   } catch (err) {
+//     return next({
+//       log: 'Error in getScoreboard',
+//       status: 500,
+//       message: { err: 'Failed to fetch scoreboard' },
+//     });
+//   }
+// };
 export default userController;
